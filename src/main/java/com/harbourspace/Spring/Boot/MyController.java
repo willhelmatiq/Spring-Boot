7f01998992b1aa79fe70312f6869c3fd1e2225cc
willhelmatiq <rot3ad@gmail.com>
2023-02-15 09:09:30 +0100

    test gradle-publish

---
+++ src/main/java/com/harbourspace/Spring/Boot/MyController.java
@@ -0,0 +1,20 @@
+package com.harbourspace.Spring.Boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/grade/{letter}")
+    public int gradeLetter(@PathVariable String letter) {
+        if (("A".equals(letter))) {
+            return 100;
+        } else if ("B".equals(letter)) {
+            return 90;
+        } else {
+            return 0;
+        }
+    }
+}
